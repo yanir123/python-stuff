cbf2aef5e88a07fbebb6a26958a0a8489252ab49
Yanir Harel <yanir44321@gmail.com>
2023-01-24 17:09:13 +0200

    style(): document code

--- clustering/TBAG/TrajectoryClustering.py
+++ clustering/TBAG/TrajectoryClustering.py
@@ -6,9 +6,6 @@ from numpy import ctypeslib as npct
 
 
 class TBAG(object):
-    """
-        Api class to use the trajectory algorithm
-    """
 
     def __init__(self,
                  eps: float = 200,
@@ -17,6 +14,28 @@ class TBAG(object):
                  max_speed: float = 300,
                  min_speed: float = 100,
                  window: int = 4) -> None:
+        """Class that is used as an API to run the underlying c code
+
+        Parameters
+        ----------
+        eps : float, optional
+            maximum distance in `meters` between the end of one cluster and the start of another, by default 200
+        alpha : float, optional
+            maximum variance in the azimuth between the `window` last points and the `window` first points in the combining clusters, by default 2500
+        time_eps : float, optional
+            maximum time diferrence when the first cluster ends and the second starts, by default np.inf
+        max_speed : float, optional
+            maximum speed allowed, by default 300
+        min_speed : float, optional
+            minimum speed allowed, by default 100
+        window : int, optional
+            the number of points to take from the start and end of the combining clusters, by default 4
+
+        Raises
+        ------
+        OSError
+            If the c library was not found
+        """
         try:
             self.c_trajectory_clustering_function = npct.load_library(
                 r'trajectory_clustering.dll',
@@ -54,6 +73,25 @@ class TBAG(object):
             alt_col: str = 'alt',
             timestamp_col: str = 'timestamp',
             cast_to_radians: bool = False) -> None:
+        """Function that will fit the data in the model
+
+        Parameters
+        ----------
+        data : np.ndarray | pd.DataFrame
+            either numpy array or pandas dataframe containing the data.\n
+            if `np.ndarray` it should be of size (n, 4) where n is the number of data points and 4 representing (lat, lon, alt, timestamp).\n
+            if `pd.DataFrame` the *_col parameters represent the keys that hold the relevant data.
+        lat_col : str, optional
+            column name of the latitude value, by default 'lat'
+        lon_col : str, optional
+            column name of the longitude value, by default 'lon'
+        alt_col : str, optional
+            column name of the altitude value, by default 'alt'
+        timestamp_col : str, optional
+            column name of the timestamp value, by default 'timestamp'
+        cast_to_radians : bool, optional
+            condition that indicates if the data should be casted to radians, by default False
+        """
         TBAG.check_params(data, lat_col, lon_col, alt_col, timestamp_col)
 
         if isinstance(data, pd.DataFrame):
@@ -73,6 +111,13 @@ class TBAG(object):
         self.c_len = ctypes.c_size_t(self.len)
 
     def predict(self) -> np.array:
+        """Function to cluster the values into groups
+
+        Returns
+        -------
+        np.array
+            array containing the cluster ids
+        """
         res = np.zeros(self.len, dtype='int32')
 
         self.c_trajectory_clustering_function(self.data, self.c_len, self.eps,
@@ -89,12 +134,61 @@ class TBAG(object):
                     alt_col: str = 'alt',
                     timestamp_col: str = 'timestamp',
                     cast_to_radians: bool = False) -> np.array:
+        """Function that will fit the data in the model and predict on it
+
+        Parameters
+        ----------
+        data : np.ndarray | pd.DataFrame
+            either numpy array or pandas dataframe containing the data.\n
+            if `np.ndarray` it should be of size (n, 4) where n is the number of data points and 4 representing (lat, lon, alt, timestamp).\n
+            if `pd.DataFrame` the *_col parameters represent the keys that hold the relevant data.
+        lat_col : str, optional
+            column name of the latitude value, by default 'lat'
+        lon_col : str, optional
+            column name of the longitude value, by default 'lon'
+        alt_col : str, optional
+            column name of the altitude value, by default 'alt'
+        timestamp_col : str, optional
+            column name of the timestamp value, by default 'timestamp'
+        cast_to_radians : bool, optional
+            condition that indicates if the data should be casted to radians, by default False
+
+        Returns
+        -------
+        np.array
+            array containing the cluster ids
+        """
         self.fit(data, lat_col, lon_col, alt_col, timestamp_col,
                  cast_to_radians)
         return self.predict()
 
     @staticmethod
-    def check_params(data, lat_col, lon_col, alt_col, timestamp_col):
+    def check_params(data: np.ndarray | pd.DataFrame, lat_col: str,
+                     lon_col: str, alt_col: str, timestamp_col: str):
+        """Function to check if parameters are valid
+
+        Parameters
+        ----------
+        `data` : np.ndarray | pd.DataFrame
+            either numpy array or pandas dataframe containing the data.\n
+            if `np.ndarray` it should be of size (n, 4) where n is the number of data points and 4 representing (lat, lon, alt, timestamp).\n
+            if `pd.DataFrame` the *_col parameters represent the keys that hold the relevant data.
+        `lat_col` : str
+            colomn name of the latitude value
+        `lon_col` : str
+            column name of the longitude value
+        `alt_col` : str
+            column name of the altitude value
+        `timestamp_col` : str
+            column name o te timestamp value
+
+        Raises
+        ------
+        `ValueError`
+            if any given column name is not in the dataframe
+        `ValueError`
+            if given data parameter is not np.ndarray or pd.DataFrame
+        """
         if isinstance(data, pd.DataFrame):
             for col in (lat_col, lon_col, alt_col, timestamp_col):
                 if col not in data.columns:
